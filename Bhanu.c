fb6b2e463abb1fd44110c15b6fb3587135dd2e47
bhanuprakashgattu <39763364+bhanuprakashgattu@users.noreply.github.com>
2019-05-08 15:55:10 +0530

    Update Bhanu.c

--- Bhanu.c
+++ Bhanu.c
@@ -4,13 +4,13 @@ int main()
     int n1=10, n2=25, n3=65;
 
     if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
+        printf("%d is the largest number.", n1);
 
     if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
+        printf("%d is the largest number.", n2);
 
     if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
+        printf("%d is the largest number.", n3);
 
     return 0;
 }
